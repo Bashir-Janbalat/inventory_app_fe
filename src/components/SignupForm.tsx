de49720cc8f1949a874d56548807f102159a06a4
Bashir Janbalat <bashirjanbalat@gmail.com>
2025-04-27 17:55:09 +0200

    Refactor SignupForm to use Formik and Yup for validation

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -3,6 +3,21 @@ import {UserDTO} from "../types/UserDTO";
 import {Button, Container, Grid, Link, Paper, TextField, Typography} from "@mui/material";
 import {Link as RouterLink, useNavigate} from "react-router-dom";
 import {signup} from "../auth/AuthApi.tsx";
+import * as Yup from "yup";
+import {Field, Form, Formik} from "formik";
+
+const validationSchema = Yup.object({
+    name: Yup.string().required("Name is required"),
+    username: Yup.string().required("Username is required"),
+    email: Yup.string().email("Invalid email format").required("Email is required"),
+    password: Yup.string()
+        .min(8, "Password must be at least 8 characters long")
+        .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+        .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+        .matches(/\d/, "Password must contain at least one number")
+        .matches(/[@#$%^&+=]/, "Password must contain at least one special character")
+        .required("Password is required"),
+});
 
 const initialUser: UserDTO = {
     name: "",
@@ -12,120 +27,123 @@ const initialUser: UserDTO = {
 };
 
 const SignUpForm: React.FC = () => {
-    const [user, setUser] = useState<UserDTO>(initialUser);
-    const [errors, setErrors] = useState<{ [key: string]: string }>({});
     const [loading, setLoading] = useState<boolean>(false);
     const [message, setMessage] = useState<string>("");
     const navigate = useNavigate();
 
-    const validateForm = (values: UserDTO) => {
-        const errors: { [key: string]: string } = {};
-        if (!values.name) errors.name = "Name is required";
-        if (!values.username) errors.username = "Username is required";
-        if (!values.email) {
-            errors.email = "Email is required";
-        } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-            errors.email = "Invalid email format";
-        }
-        if (!values.password) errors.password = "Password is required";
-        else if (values.password.length < 8) errors.password = "Password must be at least 8 characters long";
-        else if (!/[A-Z]/.test(values.password)) errors.password = "Password must contain at least one uppercase letter";
-        else if (!/[a-z]/.test(values.password)) errors.password = "Password must contain at least one lowercase letter";
-        else if (!/\d/.test(values.password)) errors.password = "Password must contain at least one number";
-        else if (!/[@#$%^&+=]/.test(values.password)) errors.password = "Password must contain at least one special character";
+    const handleSubmit = async (values: UserDTO) => {
+        setLoading(true);
+        setMessage("");
+        try {
+            const response = await signup(values);
 
-        return errors;
-    };
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
-        setUser((prevState) => ({...prevState, [name]: value}));
-    };
-
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        const validationErrors = validateForm(user);
-        if (Object.keys(validationErrors).length === 0) {
-            setErrors(validationErrors);
-            setLoading(true);
-            setMessage("");
-            try {
-                const response = await signup(user);
-
-                if (response.statusCode === 201) {
-                    setMessage("Account created successfully! Redirecting to login...");
-                    setTimeout(() => {
-                        navigate("/login");
-                    }, 2000);
-                } else {
-                    setMessage(response.message);
-                }
-            } catch (error) {
-                setMessage("Registration failed." + error);
-            } finally {
-                setLoading(false);
+            if (response.statusCode === 201) {
+                setMessage("Account created successfully! Redirecting to login...");
+                setTimeout(() => {
+                    navigate("/login");
+                }, 2000);
+            } else {
+                setMessage(response.message);
             }
-        } else {
-            setErrors(validationErrors);
+        } catch (error) {
+            setMessage("Registration failed." + error);
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
         <Container component="main" maxWidth="sm">
             <Paper elevation={3} sx={{padding: 3}}>
-                <Typography variant="h5" align="center" gutterBottom> Create a new account </Typography>
-                <form onSubmit={handleSubmit}>
-                    <Grid container spacing={2} direction="column">
-                        <TextField
-                            name="name"
-                            label="Name"
-                            value={user.name}
-                            onChange={handleChange}
-                            fullWidth
-                            variant="outlined"
-                            error={Boolean(errors.name)}
-                            helperText={errors.name}
-                        />
-                        <TextField
-                            name="username"
-                            label="Username"
-                            value={user.username}
-                            onChange={handleChange}
-                            fullWidth
-                            variant="outlined"
-                            error={Boolean(errors.username)}
-                            helperText={errors.username}
-                        />
-                        <TextField
-                            name="email"
-                            label="Email"
-                            value={user.email}
-                            onChange={handleChange}
-                            fullWidth
-                            variant="outlined"
-                            error={Boolean(errors.email)}
-                            helperText={errors.email}
-                        />
-                        <TextField
-                            name="password"
-                            label="Password"
-                            value={user.password}
-                            onChange={handleChange}
-                            type="password"
-                            fullWidth
-                            variant="outlined"
-                            error={Boolean(errors.password)}
-                            helperText={errors.password}
-                        />
-                        <Button type="submit" fullWidth variant="contained" color="primary" disabled={loading}>
-                            {loading ? "Signing Up..." : "Sign Up"}
-                        </Button>
-                    </Grid>
-                </form>
+                <Typography variant="h5" align="center" gutterBottom>
+                    Create a new account
+                </Typography>
+                <Formik
+                    initialValues={initialUser}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}
+                >
+                    {({isSubmitting, isValid, dirty, touched, errors}) => (
+                        <Form>
+                            <Grid container spacing={2} direction="column">
+                                <Field
+                                    name="name"
+                                    label="Name"
+                                    as={TextField}
+                                    fullWidth
+                                    variant="outlined"
+                                    error={touched.name && Boolean(errors.name)}
+                                    helperText={touched.name && errors.name}
+                                    InputProps={{
+                                        style: {
+                                            borderColor: touched.name && !errors.name ? 'green' : '',
+                                        }
+                                    }}
+                                />
+                                <Field
+                                    name="username"
+                                    label="Username"
+                                    as={TextField}
+                                    fullWidth
+                                    variant="outlined"
+                                    error={touched.username && Boolean(errors.username)}
+                                    helperText={touched.username && errors.username}
+                                    InputProps={{
+                                        style: {
+                                            borderColor: touched.username && !errors.username ? 'green' : '',
+                                        }
+                                    }}
+                                />
+                                <Field
+                                    name="email"
+                                    label="Email"
+                                    as={TextField}
+                                    fullWidth
+                                    variant="outlined"
+                                    error={touched.email && Boolean(errors.email)}
+                                    helperText={touched.email && errors.email}
+                                    InputProps={{
+                                        style: {
+                                            borderColor: touched.email && !errors.email ? 'green' : '',
+                                        }
+                                    }}
+                                />
+                                <Field
+                                    name="password"
+                                    label="Password"
+                                    as={TextField}
+                                    type="password"
+                                    fullWidth
+                                    variant="outlined"
+                                    error={touched.password && Boolean(errors.password)}
+                                    helperText={touched.password && errors.password}
+                                    InputProps={{
+                                        style: {
+                                            borderColor: touched.password && !errors.password ? 'green' : '',
+                                        }
+                                    }}
+                                />
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    disabled={isSubmitting || loading || !(isValid && dirty)}
+                                >
+                                    {loading ? "Signing Up..." : "Sign Up"}
+                                </Button>
+                            </Grid>
+                        </Form>
+                    )}
+                </Formik>
 
                 {message && (
-                    <Typography variant="body2" color={message.includes("failed") ? "error" : "success"} align="center"
-                                sx={{marginTop: 2}}>
+                    <Typography
+                        variant="body2"
+                        color={message.includes("failed") ? "error" : "success"}
+                        align="center"
+                        sx={{marginTop: 2}}
+                    >
                         {message}
                     </Typography>
                 )}
