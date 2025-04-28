081f358e4df21eaa269acd05be7d0e452a05a5b4
Bashir Janbalat <bashirjanbalat@gmail.com>
2025-04-29 01:05:27 +0200

    Refactor ProductList to simplify and externalize state management.

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,130 +1,50 @@
-import React, {useEffect, useState} from 'react';
-import {useNavigate} from 'react-router-dom';
-import {CircularProgress, Container, Grid, Pagination, Stack,} from '@mui/material';
-import {ProductDTO} from '../types/ProductDTO';
-import {BrandDTO} from "../types/BrandDTO.ts";
-import {CategoryDTO} from "../types/CategoryDTO.ts";
-import {SupplierDTO} from "../types/SupplierDTO.ts";
-import {getProducts} from '../api/ProductApi.ts';
-import {getCategories} from "../api/CategoryApi.ts";
-import {getBrands} from "../api/BrandApi.ts";
+import React from 'react';
+import {Grid, Pagination, Stack} from '@mui/material';
 import ProductCard from './ProductCard';
-import ProductFilters from "./filters/ProductFilters.tsx";
-import {getSuppliers} from "../api/SupplierApi.ts";
+import {ProductDTO} from "../types/ProductDTO.ts";
+import {useNavigate} from "react-router-dom";
 
-const ProductList: React.FC = () => {
-    const [products, setProducts] = useState<ProductDTO[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
+
+interface ProductsGridProps {
+    products: ProductDTO[];
+    totalPages: number;
+    page: number;
+    setPage: (value: number) => void;
+}
+
+const ProductList: React.FC<ProductsGridProps> = ({
+                                                      products,
+                                                      totalPages,
+                                                      page,
+                                                      setPage,
+                                                  }) => {
     const navigate = useNavigate();
-    const [sortBy, setSortBy] = useState<'name' | 'price'>('name');
-    const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
-    const [searchBy, setSearchBy] = useState<string>('');
-    const [categories, setCategories] = useState<CategoryDTO[]>([]);
-    const [suppliers, setSuppliers] = useState<SupplierDTO[]>([]);
-    const [brands, setBrands] = useState<BrandDTO[]>([]);
-    const [categoryName, setCategoryName] = useState<string>('');
-    const [brandName, setBrandName] = useState<string>('');
-    const [supplierName, setSupplierName] = useState<string>('');
-    const size = 4;
-
-    const fetchInitialData = async () => {
-        setLoading(true);
-        try {
-            const [categoriesResponse, brandsResponse, suppliersResponse] = await Promise.all([
-                getCategories(),
-                getBrands(),
-                getSuppliers()
-            ]);
-            setCategories(categoriesResponse.content);
-            setBrands(brandsResponse.content);
-            setSuppliers(suppliersResponse.content)
-        } catch (error) {
-            console.error('Error fetching initial data:', error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const fetchProducts = async () => {
-        setLoading(true);
-        try {
-            const productResponse =
-                await getProducts(page - 1, size, sortBy, sortDirection, searchBy, categoryName, brandName, supplierName);
-            setProducts(productResponse.content);
-            setTotalPages(productResponse.totalPages);
-        } catch (error) {
-            console.error('Error fetching products:', error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        fetchProducts();
-    }, [page, sortBy, sortDirection, searchBy, categoryName, brandName, supplierName]);
-
-
-    useEffect(() => {
-        fetchInitialData();
-    }, []);
-
 
     const handleProductClick = (id: number) => {
         navigate(`/products/${id}`);
     };
 
     return (
-        <Container
-            sx={{py: 4,mt:{lg: 2,md:2 ,xs: 0},
-                backgroundColor: 'background.default', minHeight: '100vh', overflow: 'auto', height: '100%'}}>
-            <ProductFilters
-                sortBy={sortBy}
-                sortDirection={sortDirection}
-                searchBy={searchBy}
-                categoryName={categoryName}
-                brandName={brandName}
-                categories={categories}
-                brands={brands}
-                supplierName={supplierName}
-                suppliers={suppliers}
-                setSortBy={setSortBy}
-                setSortDirection={setSortDirection}
-                setSearchBy={setSearchBy}
-                setCategoryName={setCategoryName}
-                setBrandName={setBrandName}
-                setSupplierName={setSupplierName}
-                setPage={setPage}
-            />
-            {
-                loading ? (<Grid container justifyContent="center"><CircularProgress/></Grid>) : (
-                    <>
-                        <Grid container spacing={4} sx={{minHeight: '400px'}}>
-                            {products.map((product) => (
-                                <Grid size={3} key={product.id}>
-                                    <ProductCard product={product} onClick={() => handleProductClick(product.id)}/>
-                                </Grid>
-                            ))}
-                        </Grid>
-                        <Stack alignItems="center" mt={4}>
-                            <Pagination
-                                count={totalPages}
-                                page={page}
-                                onChange={(_event, value) => setPage(value)}
-                                color="primary"
-                                sx={{
-                                    '&:hover': {
-                                        transform: 'scale(1.02)',
-                                        boxShadow: 6,
-                                        cursor: 'pointer'
-                                    },
-                                }}
-                            />
-                        </Stack>
-                    </>
-                )}
-        </Container>
+        <>
+            <Grid container spacing={4} style={{minHeight: '400px'}}>
+                {products.map((pro) => (
+                    <Grid size={{xs: 12, sm: 6, md: 4}} key={pro.id}>
+                        <ProductCard product={pro} onClick={() => handleProductClick(pro.id)}/>
+                    </Grid>
+                ))}
+            </Grid>
+
+            <Stack direction="row" justifyContent="center" alignItems="center" marginTop={4}>
+                <Pagination
+                    count={totalPages}
+                    page={page}
+                    onChange={(_event, value) => setPage(value)}
+                    color="primary"
+                    variant="outlined"
+                    shape="rounded"
+                />
+            </Stack>
+        </>
     );
 };
 
